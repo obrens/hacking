25d3b3d822abce4cd07701bc9fe892133e834f9c
obren <starovicoo@gmail.com>
2021-02-12 16:24:16 +0100

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    GameManager Instance;
+    
+    void Awake() {
+        if(Instance == null) {
+            Instance = this;
+        } else if(Instance != this) {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(gameObject);
+        EventManager.StartListening("start level", StartLevel);
+    }
+
+    private void OnDestroy() {
+        EventManager.StopListening("start level", StartLevel);
+    }
+    
+    private void StartLevel() {
+        SceneManager.LoadScene("Level");
+    }
+}
